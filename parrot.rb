46c68772befed65203c13e1123da3bb845fde7d7
immutable-integer-8885 <gidelbusto@gmail.com>
2020-05-10 01:40:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(*p, a = "Squawk!")
-  puts a
+def parrot(*p, squawk = "Squawk!")
+  return squawk
 end
   
